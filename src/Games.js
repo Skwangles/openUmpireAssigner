0c647d6b7daccf1dce18509a57476278a2633196
A.S <37290813+Skwangles@users.noreply.github.com>
2023-05-01 13:40:16 +1200

    updateGameValue function to allow real time updates of umpires

--- src/Games.js
+++ src/Games.js
@@ -5,12 +5,30 @@ import "./Games.css"
  
 
 function Games({getGames, setGames, setUsedUmpires, gameLength, setSelectedGame}) {
+
+    let updateGameValue = (key, newGame) => {
+        let indexOfGame = -1;
+        for(const index in getGames)
+        {
+            let item = getGames[index]
+            if(item.Time + "|" + item.Turf === key)
+            {
+                indexOfGame = index
+                break;
+            }
+        }
+        if(indexOfGame < 0) return false
+        getGames[indexOfGame] = newGame
+        setGames(getGames)
+    }
+
+
   return (
     <div className="Games" style={{border: '1px dashed red'}}>
         <table>
             <tbody>
-                {getGames.map(info => <Game key={info.A + info.B} game={info} setSelectedGame={setSelectedGame}></Game> )}
-                <Game game={{"A":"--", "B":"--", "Time":"00:00"}} setSelectedGame={setSelectedGame}></Game>
+                {getGames.map(info => <Game key={info.Time + "|" + info.Turf} game={info} setSelectedGame={setSelectedGame}></Game> )}
+                <Game game={{"A":"--", "B":"--", "Time":"00:00"}} setSelectedGame={setSelectedGame} updateGameValue={updateGameValue}></Game>
             </tbody>
         </table>
     </div>
