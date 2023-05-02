44a8980203891a5eabbe3ebe74669aac366d7e8e
A.S <37290813+Skwangles@users.noreply.github.com>
2023-05-02 14:03:57 +1200

    Adding focusing of 'umpire' row

--- src/Umpire.js
+++ src/Umpire.js
@@ -1,10 +1,16 @@
 import { Draggable } from "react-drag-and-drop";
 
 function Umpire(props) {
-    let {name, canMens, canWomens} = props.info
+    
+  let handleClickToFocus = (data, event) => {
+    props.setSelectedUmpire(props.info)
+  }
+  
+  let {name, canMens, canWomens} = props.info
+
   return (
-    <Draggable type="umpire" data={JSON.stringify(props.info)}>
-        <p>{name}: {canMens ? "M" : "-"}/{canWomens ? "W" : "-"}</p>
+    <Draggable type="umpire" data={JSON.stringify(props.info)} onClick={handleClickToFocus}>
+        {name}:{canMens ? "M" : "-"}/{canWomens ? "W" : "-"}
     </Draggable>
   );
 }
