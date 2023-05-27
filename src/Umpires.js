da684de9d65adadd7d2e8e9975f0f8cb5bbcc3b2
skwangles <tree_house@Hotmail.co.nz>
2023-05-27 15:15:22 +1200

    Adds disabling, and reason to highlighting - also adds multiple columns

--- src/Umpires.js
+++ src/Umpires.js
@@ -5,19 +5,41 @@ import { gameToId } from './utils';
 function Umpires({umpires, highlightType, setSelectedUmpire, selectedGame}) {
 
 
-  let filteredUmpires = umpires;
-
+  // Disable umpires by game - by highlight mode - GAMES is 'UNAVAILABLE GAMES'
   if(highlightType === "game" && selectedGame.hasOwnProperty("Time")){
-    console.info("Umpires disabled")
-    // Disable umpires unavailable
-    filteredUmpires = umpires.map(umpire => { return {isDisabled: !umpire.games.some(game => gameToId(game) === gameToId(selectedGame)), ...umpire}})
+    umpires = umpires.map(umpire => { 
+
+      // Check if umpire disabled for game & attach reason
+      console.log("Umpire not iterable")
+      console.log(umpire)
+      for (const game of umpire.games){
+        if (gameToId(game) === gameToId(selectedGame))
+          return {...umpire, isDisabled: true, reason: game.reason || "None"}
+      }
+
+      return {...umpire, isDisabled: false}
+    })
+
   }
 
-  let umpComponents = filteredUmpires.length > 0 ? filteredUmpires.map(umpire => <Umpire key={umpire.name} info={umpire} setSelectedUmpire={setSelectedUmpire}></Umpire>) : <td>No umpires Found</td>
+  // Create Table rows
+  let umpComponents = umpires.length > 0 ? umpires.map(umpire => <Umpire key={umpire.name} info={umpire} setSelectedUmpire={setSelectedUmpire}></Umpire>) : <tr>No umpires Found</tr>
 
   return (
     <table className=''>
+
+    {/* Table headers */}
+    <thead>
+        <th>Name</th>
+        <th>M/W</th>
+        <th>Skill</th>
+        <th>Teams</th>
+        <th>Club</th>
+        <th>Notes</th>
+      </thead>
       <tbody>
+      
+      {/* Umpire rows*/}
         {umpComponents}
         </tbody>
     </table>
