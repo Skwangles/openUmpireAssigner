18559a7d60548247771803fa27f32f2befd03226
A.S <37290813+Skwangles@users.noreply.github.com>
2023-06-06 13:23:35 +1200

    Adding game parsing

--- src/App.js
+++ src/App.js
@@ -89,29 +89,45 @@ function App() {
     "restrictedTurf": ["St Pauls"]
   }]);
 
-  const [csvData, setCSVData] = useState([]);
+  const [teamData, setTeamData] = useState([]);
 
-  const handleFileUpload = (event) => {
+  const handleTeamsUpload = (event) => {
     const file = event.target.files[0];
 
     // Parse contents
     Papa.parse(file, {
       header: true,
       complete: (results) => {
-        setCSVData(results.data);
+        setTeamData(results.data);
       },
     });
+  };
 
-    // Validate
-    // if (csvData.length > 0 && csvData[0].hasOwnProperty("Team A") && csvData[0].hasOwnProperty("Team A") && csvData[0].hasOwnProperty("Team A") && csvData[0].hasOwnProperty("Team A")){}
+  const handleGamesUpload = (event) => {
+    const file = event.target.files[0];
 
-    // "A": "Morrinsville",
-    //   "B": "Old boys",
-    //   "Time": "16:00",
-    //   "Turf":"GHC1",
-    //   "Grade":"M2",
-    //   "ump1":null,
-    //   "ump2":null
+    // Parse contents
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        let games = []
+        for (const game of results.data){
+         
+          // Categorise game grade by teams list
+          const team = teamData.find(team => team.Name == game.A || team.Name == game.B);
+          if(team == undefined || !team.hasOwnProperty("Grade")){
+            console.log("Invalid teams in game, or team object malformed: " + JSON.stringify(game))
+            continue;
+          }
+
+          //Add extra entries to game info
+          games.push({...game, "Grade": team.Grade, ump1: null, ump2: null})
+        }
+
+        setGames(games);
+      },
+    });
   };
   
   let [parsedUmpires, setParsedUmpires] = useState(umpires.map(umpire => { return { ...umpire, "blockedGames": parseUmpire(umpire, games, gameLength_min) } }))
@@ -122,7 +138,12 @@ function App() {
       {/* Upload of games CSV*/}
       <div>
         Enter Games:
-        <input type="file" accept=".csv" onChange={handleFileUpload} />
+        <input type="file" accept=".csv" onChange={handleTeamsUpload} />
+      </div>
+
+      <div>
+        Enter Team Names:
+        <input type="file" accept=".csv" onChange={handleTeamsUpload} />
       </div>
 
       <h4>Disabling for: {highlightType === "game" ? (selectedGame.hasOwnProperty("A") ? gameToId(selectedGame) : "None selected") : selectedUmpire.hasOwnProperty("name") ? selectedUmpire.name : "None Selected"}</h4>
