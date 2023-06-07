83ba56981c65824012cb28e4acf2c6869863690b
A.S <37290813+Skwangles@users.noreply.github.com>
2023-06-07 12:06:25 +1200

    Utils functions

--- src/utils.js
+++ src/utils.js
@@ -10,4 +10,43 @@ function umpireToId(info) {
     return info.Name + "|" + info.Levels.join(",")
 }
 
-export { gameToId, umpireToId }
+const csvToGame = (game) => {
+    return {
+      "A": game["home team"],
+      "B": game["away team"],
+      "Time": game["game time"],
+      "Turf": stripToTurf(game["playing surface"]),
+      "Date": game["game date"],
+      "Round": game["round"],
+      "Grade": game["grade"].substring(0, 3).replace("R", ""), //e.g. MR3 Name1 Name2 -> M3
+      ump1: null, 
+      ump2: null
+    }
+  }
+  
+  const csvToUmpire = (umpire) => {
+    return {
+      "Name": umpire["Name"],
+      "Teams": umpire["Teams"].split(/,\s+/),
+      "Levels": umpire["Levels"].split(/,\s+/),
+      "RestrictedTurf": umpire["Restricted Turfs"].split(/,\s+/),
+      "Club": umpire["Club"],
+      "TBAO": umpire["To be aware of"],
+      "Notes":umpire["Notes"]
+    }
+  }
+
+  const stripToTurf = (name) => {
+    switch(name){
+      case "New World Te Rapa Turf 1":
+        return "Turf 1"
+      case "Lugtons Turf 2":
+        return "Turf 2"
+      case "St Pauls Collegiate":
+        return "St Pauls"
+      default:
+        return name
+    }
+  }
+
+export { gameToId, umpireToId, csvToGame, csvToUmpire }
