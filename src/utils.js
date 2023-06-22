4f023834f49ee16ede0ae102d231568bb3fe530a
skwangles <tree_house@Hotmail.co.nz>
2023-06-22 17:29:21 +1200

    handling zero-padded date parts by converting to a number

--- src/utils.js
+++ src/utils.js
@@ -71,25 +71,32 @@ const csvToGame = (game) => {
     return a.Grade.localeCompare(b.Grade)
   }
 
-  function dateStringComparison(a, b, subpredicate){
-    // -1 means it is higher (e.g. A of A-Z)
-    
-    if (a.Date === b.Date) 
-      {
-       return subpredicate(a, b)
-      }
-    
-    // Checking Date (they aren't equal)  
+  function dateStringComparison(a, b, subpredicate = ()=>0){
+    // -1 means it is higher (e.g. A > Z)
+    const A_HIGHER = -1
+    const A_LOWER = 1
+
+    // Quick exit
+    if (a.Date === b.Date) return subpredicate(a, b)
+
     let partsA = a.Date.split("/")
     let partsB = b.Date.split("/")
 
-    if (partsA[2] !== partsB[2])
-      return Number(partsA[2]) < Number(partsB[2]) ? -1 : 1 //Smaller year, comes higher
+    // Year
+    if (partsA.length > 2 && partsB.length > 2 /* Assume year same if not specified */ && Number(partsA[2]) !== Number(partsB[2]))
+      return Number(partsA[2]) < Number(partsB[2]) ? A_HIGHER : A_LOWER
       
-    if (partsA[1] !== partsB[1])
-      return Number(partsA[1]) < Number(partsB[1]) ? -1 : 1
+  
+    // Month  
+    if (Number(partsA[1]) !== Number(partsB[1])) 
+      return Number(partsA[1]) < Number(partsB[1]) ? A_HIGHER : A_LOWER
 
-    return Number(partsA[0]) < Number(partsB[0]) ? -1 : 1
+    // Day  
+    if (Number(partsA[0]) !== Number(partsB[0]))
+      return Number(partsA[0]) < Number(partsB[0]) ? A_HIGHER : A_LOWER
+    
+    // If all of the parts match - must be equal
+    return subpredicate(a, b)
   }
 
 
