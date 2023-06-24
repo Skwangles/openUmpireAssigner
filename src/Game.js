c48a16d25b07124137eb96deb3fae0bd56221ea3
A.S <37290813+Skwangles@users.noreply.github.com>
2023-06-24 16:02:58 +1200

    remove game as a dependency (we don't want it forcing an effect update)

--- src/Game.js
+++ src/Game.js
@@ -57,7 +57,7 @@ function Game({ game, updateGameValue, setSelectedGame, id }) {
       // Update overall storage of games
       updateGameValue(id, {...game, ump1:ump1, ump2:ump2});
     }
-  }, [id, game, ump1, ump2, updateGameValue]);
+  }, [id, ump1, ump2, updateGameValue]);
 
   return (
     <tr
