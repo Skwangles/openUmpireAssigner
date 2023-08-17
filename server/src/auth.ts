5fc852d36e1c0e18a9513b2d929a371e14315b97
Xander Stokes <xander.c.stokes@gmail.com>
2023-08-17 13:36:27 +1200

    Adding a hash function to auth.ts

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,24 +1,29 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import { Request, Response, NextFunction } from "express";
 
 // Function to check if the provided password matches the hashed password
-const verifyPassword = async (password, hashedPassword) => {
+const verifyPassword = async (password: string, hashedPassword: string) => {
+  const hash = hashString(password);
   return await bcrypt.compare(password, hashedPassword);
 };
 
 // Middleware to check if user is authenticated
-const isAuthenticated = (req, res, next) => {
-  next();
-  return; //DEBUG ONLY!!!
-  //TODO:Remove login override
-
-  // if (req.session.isAuthenticated) {
-  //   next();
-  // } else {
-  //   res.status(401).json({ error: 'Authentication required' });
-  // }
+const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session.authenticated) {
+    next();
+  } else {
+    res.status(401).json({ error: "Authentication required" });
+  }
 };
 
-export default {
-  verifyPassword,
-  isAuthenticated
+// Function to hash a string using bcrypt
+const hashString = async (inputString: string, saltRounds = 10) => {
+  try {
+    const hashedString = await bcrypt.hash(inputString, saltRounds);
+    return hashedString;
+  } catch (error) {
+    throw error;
+  }
 };
+
+export { hashString, verifyPassword, isAuthenticated };
