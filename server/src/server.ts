c2d38e3e3419b930ecc52c8858834b059ebeb21b
Xander Stokes <xander.c.stokes@gmail.com>
2023-08-17 13:37:08 +1200

    Add login to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import session from "express-session";
+import userRoutes from "./users";
 import cors from "cors";
 import { json } from "body-parser";
 import routesRouter from "./routes";
+import { UserAccount } from "database";
+import { verifyPassword } from "./auth";
+import db from "./db";
 
 const app = express();
 const port = 5000;
@@ -27,6 +31,38 @@ app.use(
 
 // Parse incoming request data as JSON
 app.use(json());
+
+// Login endpoint
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the user exists in the 'users' table
+  const user: UserAccount = await new Promise((resolve, reject) => {
+    db.get(
+      "SELECT * FROM users WHERE username = ?",
+      [username],
+      (err, row: any) => {
+        if (err) reject(err);
+        resolve(row);
+      }
+    );
+  });
+
+  if (user && (await verifyPassword(password, user.password_hash))) {
+    // Set isAuthenticated flag in the session
+    req.session.authenticated = true;
+    req.session.user = user;
+
+    res.json({ ok: true, message: "Login successful" });
+
+    return;
+  }
+
+  // Default deny
+  res.status(401).json({ error: "Invalid username or password" });
+});
+
+app.use("/api/users", userRoutes);
 
 // Set up API routes
 app.use("/api", routesRouter);
